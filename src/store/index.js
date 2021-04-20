34d308d5093690932782805e5eb9e7eba9c5308f
Mathias Hammer <rabbyraptor@hotmail.com>
2021-04-20 14:10:22 +0200

    Store setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,35 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
   state: {
+    data: null
   },
   mutations: {
+    setData: (state, res) => {
+      state.data = res.data;
+    }
   },
   actions: {
+    async getData({ commit, state }) {
+      if (state.data) {
+        return Promise.resolve();
+      }
+
+      return await axios
+        .get(
+          "https://api.jsonbin.io/b/607ec27524143e5df089c6c8"
+        )
+        .then((res) => commit("setData", res))
+        .catch(function (error) {
+          console.log(error);
+        })
+    }
+  },
+  getters: {
+    getDataContent: (state) => {
+      return state.data;
+    },
   },
   modules: {
   }
