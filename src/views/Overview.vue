007178242f723debb042a5d05321705d82162de9
Mathias Hammer <rabbyraptor@hotmail.com>
2021-04-29 14:15:12 +0200

    Update Overview.vue

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,15 +1,15 @@
 <template>
-    <invoices></invoices>
+  <invoices></invoices>
 </template>
 
 <script>
 // @ is an alias to /src
-import Invoices from '@/components/Invoices.vue'
+import Invoices from "@/components/Invoices.vue";
 
 export default {
-  name: 'Overview',
+  name: "Overview",
   components: {
-    Invoices
-  }
-}
+    Invoices,
+  },
+};
 </script>
