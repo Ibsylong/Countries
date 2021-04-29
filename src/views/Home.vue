2b7d60e86f0047921564237871d9015091b2c5a4
Mathias Hammer <rabbyraptor@hotmail.com>
2021-04-29 16:36:43 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,13 @@
 <template>
   <div class="home">
-    <HelloWorld />
+    <h1>Home</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
